f3012ef932c85374dcbf12f48d4615e7d8c964ad
Juan Torres <jdtorreserazo@gmail.com>
2024-10-13 17:59:46 -0700

    Styles, Fonts And Images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Inter, Poppins, Lusitana} from "next/font/google";
+
+export const inter = Inter({subsets: ["latin"]});
+export const poppins = Poppins({weight: ["200", "300", "400"], subsets: ["latin"]});
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
